94860fbed0b68e2e15fbc26707ff199d9cc4fd25
Ayush007 <ayush.rooney10@gmail.com>
2024-07-06 23:46:42 +0530

    homepage

---
+++ frontend/src/page/home.js
@@ -0,0 +1,9 @@
+import Widget from "../components/navbar.js";
+import HomePage from "../components/homepage.js";
+import React from "react";
+ const landing=()=> {
+    return (
+        <div><Widget/><HomePage/></div>
+    )
+}
+export default landing;
